421e532b28ffa73efa141bcadaf11e4c21154c3c
Nick Fitzgerald <fitzgen@gmail.com>
2016-12-13 18:08:47 -0800

    Initial commit

---
+++ build.rs
@@ -0,0 +1,19 @@
+extern crate libbindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=bz2");
+
+    let bindings = libbindgen::Builder::default()
+        .no_unstable_rust()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
